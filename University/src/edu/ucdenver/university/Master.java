b6e6f5a3df0c23e236c9a7df060c76d5e77c6353
crmoe <60358861+crmoe@users.noreply.github.com>
2020-09-30 19:16:25 -0600

    Update Master.java

--- University/src/edu/ucdenver/university/Master.java
+++ University/src/edu/ucdenver/university/Master.java
@@ -1,4 +1,14 @@
 package edu.ucdenver.university;
+import java.time.LocalDate;
 
-public class Master {
+public class Master extends Graduate{
+
+    public Master(String name, LocalDate dob){
+        super(name,dob);
+    }
+
+    @Override
+    public String getStanding(){
+        return "edu.ucdenver.university.Master";
+    }
 }
